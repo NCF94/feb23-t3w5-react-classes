f1d1b40163d3cbc83be81904e1e83c464fe537cc
Nicholas Ward <102345071+NCF94@users.noreply.github.com>
2023-09-16 11:38:33 +1000

    feat: pokemon api data being fetched

--- pokemonclass/src/App.js
+++ pokemonclass/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 
@@ -7,7 +7,8 @@ import PokemonDisplay from './PokemonDisplay';
 function App() {
   return (
     <div className="App">
-     <PokemonDisplay />
+      {/* PokemonDisplay is an instance of the component: */}
+      <PokemonDisplay />
     </div>
   );
 }
--- pokemonclass/src/PokemonDisplay.jsx
+++ pokemonclass/src/PokemonDisplay.jsx
@@ -14,11 +14,54 @@ export default class PokemonDisplay extends React.Component{
 		}
 	}
 
+	async componentDidMount(){
+		console.log("This message will appear once.");
+
+		// Grab a Pokemon and store its name 
+
+		// Generate a random number or use a user-provided number
+		function getRandomPokemonId(){
+			return Math.floor(Math.random() * 1010) + 1
+		}
+
+		// Retrieve the API data
+		let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + getRandomPokemonId()).catch(error => {
+			throw new Error("API failure.");
+		});
+
+		if (response.status === 404){
+			throw new Error("API did not have data for the requested ID.");
+		}
+
+		// Convert the response into usable JSON 
+		let data = await response.json().catch(error => {
+			throw new Error("API did not return valid JSON.");
+		}); 
+
+		// Save Pokemon name to component state 
+		this.setState({pokemonName: data.name});
+	}
+
+	componentWillUnmount(){
+		console.log("Component is being removed from the page.");
+	}
+
+	// Render is how we return JSX up to the React app
+	// so that something is displayed to the browser 
 	render(){
-		return(
-			<div>
-				<h1>Some content here</h1>
-			</div>
-		)
+
+		if (this.state.pokemonName) {
+			return (
+				<div>
+					<h1>{this.state.pokemonName}</h1>
+				</div>
+			)
+		} else {
+			return(
+				<div>
+					<h1>Loading...</h1>
+				</div>
+			)
+		}
 	}
 }
