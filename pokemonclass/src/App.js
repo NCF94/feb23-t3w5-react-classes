70bde461ff82fbd7c75d5d4e31c8bfa46f4e1719
Nicholas Ward <102345071+NCF94@users.noreply.github.com>
2023-09-16 10:52:58 +1000

    feat: class component init

---
+++ pokemonclass/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+import PokemonDisplay from './PokemonDisplay';
+
+function App() {
+  return (
+    <div className="App">
+     <PokemonDisplay />
+    </div>
+  );
+}
+
+export default App;
